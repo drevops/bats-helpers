03894ceb37c77a7e0f89140c5dfcf1e6ce0734b9
Alex Skrypnyk <alex@drevops.com>
2026-07-31 13:06:44 +1000

    Aligned 'load.bash' header and sourced 'assert.string' before its dependent.

--- load.bash
+++ load.bash
@@ -1,14 +1,15 @@
+#!/usr/bin/env bash
 ##
 # Helpers and assertions for BATS testing.
 #
 # Central loading point for all the helpers and assertions.
 # @see https://bats-core.readthedocs.io/en/stable/writing-tests.html#bats-load-library-load-system-wide-libraries
 #
-# shellcheck disable=1090
+# shellcheck disable=SC1090
 
 source "$(dirname "${BASH_SOURCE[0]}")/src/assert.base.bash"
-source "$(dirname "${BASH_SOURCE[0]}")/src/assert.command.bash"
 source "$(dirname "${BASH_SOURCE[0]}")/src/assert.string.bash"
+source "$(dirname "${BASH_SOURCE[0]}")/src/assert.command.bash"
 source "$(dirname "${BASH_SOURCE[0]}")/src/assert.file.bash"
 source "$(dirname "${BASH_SOURCE[0]}")/src/assert.git.bash"
 
